4881a45b5b07e205cb29c66e4dd9e9474e0e3277
zero5p <youyouyong25@gmail.com>
2025-04-28 21:20:31 +0900

    fix: separate client-side mobile/desktop entrypoint for Next.js app dir, resolve build error

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,8 @@
 // Next.js dynamic export: 빌드타임 프리렌더 방지
 export const dynamic = "force-dynamic";
 
-import HomeWithSession from "./HomeWithSession";
-import MobileHomeKorean from "./mobile-home-korean";
-import { useEffect, useState } from "react";
-
-function isMobile() {
-  if (typeof window === "undefined") return false;
-  return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
+import HomeEntryPoint from "./HomeEntryPoint";
 
 export default function Page() {
-  const [mobile, setMobile] = useState(false);
-
-  useEffect(() => {
-    setMobile(isMobile());
-  }, []);
-
-  if (mobile) {
-    return <MobileHomeKorean />;
-  }
-  return <HomeWithSession />;
+  return <HomeEntryPoint />;
 }
